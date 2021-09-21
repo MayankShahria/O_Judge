8b51f556d87cb2eb52410291b2f0fa68cc092ea2
MayankShahria <mayankshahria1098@gmail.com>
2021-09-21 10:15:22 +0530

    files

--- online_judge/func.py
+++ online_judge/func.py
@@ -4,8 +4,8 @@ def run_file(f):
 	with open('program.cpp', 'wb+') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
-	if os.system("g++ program.cpp -o program") == 0:
-		if os.system("timeout --preserve-status 1 ./out <TESTinput.txt > output.txt") != 0:
+	if os.system("g++ program.cpp -o program.exe") == 0:
+		if os.system("timeout --preserve-status 1 ./program.exe <TESTinput.txt > output.txt") != 0:
 			print("TLE")
 		else:
 			if os.system("diff output.txt TESToutput.txt") == 0:
